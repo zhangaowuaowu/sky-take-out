9d5f148c2bf5f2bfc0f1eba1bcf0d554b44c6440
刁卓 <zhangaowuaowu@gmail.com>
2025-02-26 14:15:09 +0800

    Your commit message here

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,9 +1,12 @@
 package com.sky.mapper;
 
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishDTO;
 import com.sky.entity.DishFlavor;
 import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -11,4 +14,14 @@ import java.util.List;
 public interface DishFlavorMapper {
     @AutoFill(OperationType.INSERT)
     void insertBatch(List<DishFlavor> flavors);
+
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
+
+    void deleteByDishIds(List<Long> dishIds);
+
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> selectByDishId(Long dishId);
+
+    void update(DishFlavor flavor);
 }
